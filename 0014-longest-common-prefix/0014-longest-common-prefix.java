ef6d393a8489f289e59c7558ea47c74d6f3b1caa
Riya Sharma <105699878+Riya-2012@users.noreply.github.com>
2024-09-06 17:37:37 +0530

    Time: 1 ms (72.96%), Space: 41.9 MB (18.09%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder result=new StringBuilder();
+        Arrays.sort(strs);
+        char[] f=strs[0].toCharArray();
+        char[] l=strs[strs.length-1].toCharArray();
+        for(int i=0;i<f.length;i++){
+            if(f[i]!=l[i])
+
+            break;
+            result.append(f[i]);
+
+            
+        
+        }
+        return result.toString();
+    }
+}
