31cf95297ca838d07bd5f4b34f488e8c0b179ba7
Riya Sharma <105699878+Riya-2012@users.noreply.github.com>
2024-09-12 18:18:56 +0530

    Time: 4 ms (56.22%), Space: 44.3 MB (45.78%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,20 +1,30 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String s2=s.toLowerCase();
-       String str = s2.replaceAll("[^a-zA-Z0-9]", "");
-    
-int i =0;
-int j=str.length()-1;
-while(i<j){
-    if(str.charAt(i)!=str.charAt(j)){
-        return false;
+        // Convert the input string to lowercase
+        String s2 = s.toLowerCase();
+        
+        // Remove all non-alphanumeric characters
+        StringBuilder cleanedStr = new StringBuilder();
+        for (char c : s2.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                cleanedStr.append(c);
+            }
+        }
+        
+        // Convert cleaned string to a character array
+        char[] chars = cleanedStr.toString().toCharArray();
+        
+        // Check for palindrome using two-pointer technique
+        int left = 0;
+        int right = chars.length - 1;
+        while (left < right) {
+            if (chars[left] != chars[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
     }
-    i++;
-    j--;
-}
-
-     return true;
-
-    }
-
 }
