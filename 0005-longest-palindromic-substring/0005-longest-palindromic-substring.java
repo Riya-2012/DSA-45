fda1e586ef6a96f8b634af1c53d30ae63b744e80
Riya Sharma <105699878+Riya-2012@users.noreply.github.com>
2024-09-18 22:07:03 +0530

    Time: 19 ms (65.82%), Space: 45.1 MB (50.47%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,37 +1,35 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if (s.length() <= 1) {
+      if (s.length() <= 1) {
             return s;
         }
+        
         String LPS = "";
-        for (int i =1; i < s.length(); i++) {
-            int low = i;
-            int high = i;
-            while (low >= 0&& high < s.length()&&s.charAt(low) == s.charAt(high)) {
-
-                low--;
-                high++;
-               
+        
+        for (int i = 0; i < s.length(); i++) {
+            // Odd-length palindromes
+            String oddPalindrome = expandAroundCenter(s, i, i);
+            if (oddPalindrome.length() > LPS.length()) {
+                LPS = oddPalindrome;
             }
 
-            String palindrome = s.substring(low + 1, high);
-            if (palindrome.length() > LPS.length()) {
-                LPS = palindrome;
-            }
-            // even
-            low = i - 1;
-            high = i;
-            while (low >= 0&& high < s.length()&&s.charAt(low) == s.charAt(high)) {
-
-                low--;
-                high++;
-              
-            }
-            palindrome = s.substring(low + 1, high);
-            if (palindrome.length() > LPS.length()) {
-                LPS = palindrome;
+            // Even-length palindromes
+            String evenPalindrome = expandAroundCenter(s, i, i + 1);
+            if (evenPalindrome.length() > LPS.length()) {
+                LPS = evenPalindrome;
             }
         }
+        
         return LPS;
     }
-}
+
+    private String expandAroundCenter(String s, int low, int high) {
+        // Expand around the center while characters match and pointers are in bounds
+        while (low >= 0 && high < s.length() && s.charAt(low) == s.charAt(high)) {
+            low--;
+            high++;
+        }
+        // Return the palindromic substring
+        return s.substring(low + 1, high);
+    }
+    }
