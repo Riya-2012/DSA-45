3fc6584b0b012555650602c5560e70565907786e
Riya Sharma <105699878+Riya-2012@users.noreply.github.com>
2024-12-14 17:58:02 +0530

    Time: 0 ms (100%), Space: 41.9 MB (17.56%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,11 +1,12 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-    s = s.trim();
-        
+        s = s.trim();
+
         // Step 2: Find the index of the last space
         int lastSpaceIndex = s.lastIndexOf(' ');
-        
-        // Step 3: The length of the last word is the distance between the last space and the end of the string
+
+        // Step 3: The length of the last word is the distance between the last space
+        // and the end of the string
         return s.length() - lastSpaceIndex - 1;
     }
 }
