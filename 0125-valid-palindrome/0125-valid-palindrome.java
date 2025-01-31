37de88e339de052d0a9095319f7027ab06b536cd
Riya Sharma <105699878+Riya-2012@users.noreply.github.com>
2025-01-31 05:36:16 -0800

    Time: 4 ms (57.12%), Space: 44.6 MB (47.16%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,9 +1,8 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // Convert the input string to lowercase
+       
         String s2 = s.toLowerCase();
         
-        // Remove all non-alphanumeric characters
         StringBuilder cleanedStr = new StringBuilder();
         for (char c : s2.toCharArray()) {
             if (Character.isLetterOrDigit(c)) {
@@ -11,10 +10,10 @@ class Solution {
             }
         }
         
-        // Convert cleaned string to a character array
+      
         char[] chars = cleanedStr.toString().toCharArray();
         
-        // Check for palindrome using two-pointer technique
+      
         int left = 0;
         int right = chars.length - 1;
         while (left < right) {
